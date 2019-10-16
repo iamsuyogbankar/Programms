693f75896897bc80bd1eab754ed91316a9a43a63
Suyog Bankar <40287058+iamsuyogbankar@users.noreply.github.com>
2019-10-16 11:02:35 +0530

    Add files via upload

---
+++ chocolate.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+	var amount = prompt("Enter amount to buy chocolate");
+	var chocolate = amount*4;
+	var wrapper = chocolate;
+	var totalChocolate = (wrapper/3)+chocolate;
+	alert("Your chocolates: "+Math.floor(totalChocolate));
+</script>
